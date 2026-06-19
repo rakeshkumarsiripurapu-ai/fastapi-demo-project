b81c9f8d76ece111955af3838fcce21cd54171bc
rakeshkumarsiripurapu-ai <rakeshkumarsiripurapu@gmail.com>
2026-06-19 09:07:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+from app.api import router
 
+app = FastAPI(title="FastAPI Demo Project")
+
+app.include_router(router)
+
+@app.get("/")
+def root():
+    return {"message": "FastAPI Demo Project is running"}
